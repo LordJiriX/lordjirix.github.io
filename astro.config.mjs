163d5e469c138f410f18dd07a57f14cf37f739eb
LordJiriX <167350618+LordJiriX@users.noreply.github.com>
2025-06-14 12:34:01 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://lordjirix.github.io',
+    site: 'https://lordjirix.netlify.app/',
   base: '/',
 });
